a1fb2af93fcf1228b291d3c66e41c543670b2132
Cesare Mauri <cesare.mauri@decathlon.com>
2023-08-25 11:28:25 +0200

    Various improvements on docs

--- README.md
+++ README.md
@@ -1,26 +1,41 @@
+# Project 
+This repo contains various hints and scenario to optimize spring boot java development:
+1. Create a size optimized docker image
+2. Test of virtual threads in terms of performance and load management
+3. Implementation of timeout budgeting
+
 # Getting Started
-Perfjava uses jdk20 with preview and incubator feature
 
+## Prerequisites
+Here the software pre-requisites for every case:
+1. slim/docker
+   * Functional docker or docker-style install
+2. virtualthreads
+   * jdk20 install
 
-## Dockerization
-Dockerization of images can be optimized using custom made JDK containing only used packages
+# Size-optimized Dockerization
+Dockerization of images can be optimized using custom-made JDK containing only used packages.
 
-### Core jar
+It's an application undependent solution. Docker run exposes a port just for showing app is working fine.
+
+## Core jar
 ```shell
 ./gradlew build
 ls -lh build/libs/*.jar | awk '{print $5, $9}'
 ```
 Jar size of opplication is: 26 Mb
 
-### Dockerfile
+## Dockerfile
 ```shell
 ./gradlew build
 
-docker build . -t perfjava:1.0.0 -f Dockerfile.plain
+docker build . -t perfjava:1.0.0 -f docker/standard/Dockerfile
 docker run -p 8081:8080 perfjava:1.0.0
+curl http://localhost:8081 
 
-docker build . -t perfjava:2.0.0 -f Dockerfile.slim
+docker build . -t perfjava:2.0.0 -f docker/slim/Dockerfile
 docker run -p 8082:8080 perfjava:2.0.0
+curl http://localhost:8082
 
 docker image ls | grep perfjava
 ```
@@ -34,20 +49,36 @@ NB: Use corretto for build in order to:
 2. use alpine machine for running, for example jre obtained from eclipse:temurin are not compatible so you need other bigger running machine to use that, debian:bookworm-slim is a candidate 
 
 ## Virtual Threads
+Plain vanilla java application running diffent test cases, after compile you can execute:
+* Out of memory test:
+  * Starts one thread after another, till system goes out of memory. In M1 system just over 4,000 threads are supported
+  * Same as before, but virtualthread are started. Not 2,800,000 threads are started, system does not crash but slows a lot. 700x load improvement is achieved.
+* Throughput test:
+  * Run N thread each executing same job (200ms sleep) and check spent time. 10,000 takes 10 seconds with 200% CPU
+  * Same as before with virtualthread. Time spent is 1 second with 700% CPU. 10x faster
+
+**Test result is: much faster with better CPU utilization and much less memory needs**
+
+* Structured concurrency:
+  * A task can be split in 2 parts. Using ScheduledThreadPoolExecutor first fail before second has finished, second finished it's work
+  * Using StructuredTaskScope.ShutdownOnFailure() as soon as first fails, second is stopped because no need to waste resources completing it because I need both to be completed
+  * Using StructuredTaskScope.ShutdownOnSuccess() as soon as first finish, second is stopped because no need to waste resources completing it because I just need one of them to be completed, case I have multiple
+
+
 ```shell
  ./gradlew compilejava
  
- java -cp ./build/classes/java/main --enable-preview net.perfjava.VirtualThreads C-T
- java -cp ./build/classes/java/main --enable-preview net.perfjava.VirtualThreads C-VT
- time java -cp ./build/classes/java/main --enable-preview net.perfjava.VirtualThreads T-T 10000
- time java -cp ./build/classes/java/main --enable-preview net.perfjava.VirtualThreads T-VT 10000
- java -cp ./build/classes/java/main --enable-preview --add-modules jdk.incubator.concurrent net.perfjava.VirtualThreads HOC
- java -cp ./build/classes/java/main --enable-preview --add-modules jdk.incubator.concurrent net.perfjava.VirtualThreads HOSC
+ java -cp ./build/classes/java/main --enable-preview net.perfjava.virtualthreads.MainExecutor C-T
+ java -cp ./build/classes/java/main --enable-preview net.perfjava.virtualthreads.MainExecutor C-VT
+ time java -cp ./build/classes/java/main --enable-preview net.perfjava.virtualthreads.MainExecutor T-T 100000
+ time java -cp ./build/classes/java/main --enable-preview net.perfjava.virtualthreads.MainExecutor T-VT 100000
+ java -cp ./build/classes/java/main --enable-preview --add-modules jdk.incubator.concurrent net.perfjava.virtualthreads.MainExecutor HOC
+ java -cp ./build/classes/java/main --enable-preview --add-modules jdk.incubator.concurrent net.perfjava.virtualthreads.MainExecutor HOSC
 ```
 
 ### Out of Memory Results
 
-Go up invocking thread till system crashes
+Go up invoking threads till system crashes
 
 | Technology      |           Max thread to crash |
 |:----------------|------------------------------:|
--- src/main/java/net/perfjava/virtualthreads/MainExecutor.java
+++ src/main/java/net/perfjava/virtualthreads/MainExecutor.java
@@ -1,5 +1,5 @@
 /* CMauri - Italy - 2023 */
-package net.perfjava;
+package net.perfjava.virtualthreads;
 
 import java.time.Duration;
 import java.util.concurrent.*;
@@ -8,7 +8,7 @@ import java.util.concurrent.locks.LockSupport;
 import java.util.stream.IntStream;
 import jdk.incubator.concurrent.StructuredTaskScope;
 
-public class VirtualThreads {
+public class MainExecutor {
     private static final AtomicInteger counter = new AtomicInteger();
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -19,18 +19,20 @@ public class VirtualThreads {
                 case "T-T" -> runLoadOfThreads(Integer.parseInt(args[1]));
                 case "T-VT" -> runLoadOfVirtualThreads(Integer.parseInt(args[1]));
                 case "HOC" -> handleOrderClassic();
+                case "HOSCF" -> handleOrderStructuredConcurrencyFailing();
                 case "HOSC" -> handleOrderStructuredConcurrency();
                 default -> System.err.println(
-                        "Please pass C-T | C-VT | T-T #threads | T-VT #Threads | HOC | HOSC");
+                        "Please pass C-T | C-VT | T-T #threads | T-VT #Threads | HOC | HOSCF |"
+                                + " HOSC");
             }
 
         } else System.err.println("Please pass a parameter ");
     }
 
-    // ----------------- OOM PART
+    // ----------------- C-T/ C-VT PART --- Out of memory
     private static void doWork() {
         int count = counter.incrementAndGet();
-        System.out.println("Thread count = " + count);
+        System.out.println("Started thread: " + count);
         LockSupport.park();
     }
 
@@ -46,10 +48,9 @@ public class VirtualThreads {
         }
     }
 
-    // -------------------- THROUGHPUT PART
+    // -------------------- T-T/ T-VT PART - THROUGHPUT Check
     private static int doThreadWork(int i) throws InterruptedException {
         Thread.sleep(Duration.ofMillis(200));
-        System.out.println(i);
         return i;
     }
 
@@ -84,38 +85,47 @@ public class VirtualThreads {
     static Integer updateInventory() throws InterruptedException {
         System.out.println("Inventory updating");
         Thread.sleep(1000);
-        if (3 == 3) throw new RuntimeException("Unexcepted error");
+        if (3 == 3) throw new RuntimeException("Unexcepted error updating inventory");
         System.out.println("Inventory updated");
         return 1000;
     }
 
-    static Integer updateOrder() throws InterruptedException {
+    static Integer updateStocks() throws InterruptedException {
         Thread.sleep(200);
         System.out.println("Order updating");
         Thread.sleep(2000);
         System.out.println("Order updated");
-        return 200;
+        return 2200;
+    }
+
+    static Integer updateStocksProviderB() throws InterruptedException {
+        Thread.sleep(200);
+        System.out.println("Order updating on provider B");
+        Thread.sleep(200);
+        System.out.println("Order updated on provider B");
+        return 400;
     }
 
     static void handleOrderClassic() throws ExecutionException, InterruptedException {
         try (var esvc = new ScheduledThreadPoolExecutor(8)) {
             Future<Integer> inventory = esvc.submit(() -> updateInventory());
-            Future<Integer> order = esvc.submit(() -> updateOrder());
+            Future<Integer> order = esvc.submit(() -> updateStocks());
 
-            int theOrder = order.get(); // Join updateOrder
-            int theInventory = inventory.get(); // Join updateInventory
+            int theOrder = order.get();
+            int theInventory = inventory.get();
 
             System.out.println("Inventory " + theInventory + " updated for order " + theOrder);
         }
     }
 
-    static void handleOrderStructuredConcurrency() throws ExecutionException, InterruptedException {
+    static void handleOrderStructuredConcurrencyFailing()
+            throws ExecutionException, InterruptedException {
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
             Future<Integer> inventory = scope.fork(() -> updateInventory());
-            Future<Integer> order = scope.fork(() -> updateOrder());
+            Future<Integer> order = scope.fork(() -> updateStocks());
 
             scope.join(); // Join both forks
-            scope.throwIfFailed(); // ... and propagate errors
+            scope.throwIfFailed();
 
             // Here, both forks have succeeded, so compose their results
             System.out.println(
@@ -123,6 +133,18 @@ public class VirtualThreads {
                             + inventory.resultNow()
                             + " updated for order "
                             + order.resultNow());
+        }
+    }
+
+    static void handleOrderStructuredConcurrency() throws ExecutionException, InterruptedException {
+        try (var scope = new StructuredTaskScope.ShutdownOnSuccess<Integer>()) {
+            scope.fork(() -> updateStocksProviderB());
+            scope.fork(() -> updateStocks());
+
+            scope.join();
+
+            // Here, both forks have succeeded, so compose their results
+            System.out.println("Stock update result: " + scope.result());
         }
     }
 }
